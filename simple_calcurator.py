3c87001df4c8ad1af0e2938b24a51f8bc4630af0
KosukeNakagawa95 <kna0891@gmail.com>
2025-12-07 16:32:28 +0900

    feat: get_vaild_numberの定義を追加。 refactor: total_minutesの式を簡素化。

--- simple_calcurator.py
+++ simple_calcurator.py
@@ -1,26 +1,19 @@
 import  math
 
-while True:
-    try:
-        time_per_unit = int(input("1個当たり何秒必要ですか？"))
-        break
+#関数get_vaild_numberを定義する
+def get_vaild_number(message): #get_vaild_numberを実行するときに(message)を表示するようにしている
+    while True: #while関数でtry exceptのループをする
+        try: #値がTrue（このばあいは半角数字が入力された）の場合の挙動
+            user_input = int(input(message)) #messageでinputされた文字列をintで整数に変換
+            return user_input #user_inputをreturnし、whileループから抜ける
 
-    except:ValueError #ValueErrorの場合のみexcept処理を行う
-    print("半角数字を入力してください。")
-    continue #tryに戻る
+        except:ValueError #ValueError（半角数字以外が入力されてエラー）の挙動
+        print("半角数字を入力してください。") #エラーメッセージをprintしtryに戻る        
+    
 
-while True:
-    try:
-        quantity = int(input("何個作りますか？"))
-        break
+time_per_unit = get_vaild_number("何個作りますか？") #time_per_unitを定義
+quantity = get_vaild_number("1個当たり何秒必要ですか？") #quantityを定義
 
-    except:ValueError
-    print("半角数字を入力してください。")
-    continue
+total_minutes = math.ceil((time_per_unit * quantity) / 60) #合計秒数を60で割って、計算結果の少数を切り上げ
 
-#所要時間を秒で求めたのち、分に計算する
-total_seconds = time_per_unit * quantity
-total_minutes = math.ceil(total_seconds / 60)
-
-#結果を表示する
 print(f"1個当たり{time_per_unit}秒かかる工程を{quantity}個では、{total_minutes}分が必要になります。")
