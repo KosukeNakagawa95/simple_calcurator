3e701079ea08a67c9f89d5e6e2b1087509d9708c
KosukeNakagawa95 <kna0891@gmail.com>
2025-12-07 16:47:02 +0900

    refactor: get_valid_numebrのスペル間違いを修正 refactor: except Valueerror:の:を修正　そのあとのインデントも修正 refactor: quantityとtime_per_unitを入れ替え

--- simple_calcurator.py
+++ simple_calcurator.py
@@ -1,19 +1,19 @@
 import  math
 
 #関数get_vaild_numberを定義する
-def get_vaild_number(message): #get_vaild_numberを実行するときに(message)を表示するようにしている
-    while True: #while関数でtry exceptのループをする
-        try: #値がTrue（このばあいは半角数字が入力された）の場合の挙動
+def get_valid_number(message): #get_vaild_numberを実行するときに(message)を表示するようにしている
+    while True: 
+        try: #エラーが起きるかもしれない処理をtry
             user_input = int(input(message)) #messageでinputされた文字列をintで整数に変換
-            return user_input #user_inputをreturnし、whileループから抜ける
+            return user_input #tryがTrueなので、whileから抜ける
 
-        except:ValueError #ValueError（半角数字以外が入力されてエラー）の挙動
-        print("半角数字を入力してください。") #エラーメッセージをprintしtryに戻る        
+        except ValueError: #ValueError（半角数字以外が入力されてエラー）の挙動
+            print("半角数字を入力してください。") #except Valueerroなのでエラーメッセージを出してtryに戻る        
     
 
-time_per_unit = get_vaild_number("何個作りますか？") #time_per_unitを定義
-quantity = get_vaild_number("1個当たり何秒必要ですか？") #quantityを定義
+quantity = get_valid_number("何個作りますか？") #time_per_unitを定義
+time_per_unit = get_valid_number("1個当たり何秒必要ですか？") #quantityを定義
 
-total_minutes = math.ceil((time_per_unit * quantity) / 60) #合計秒数を60で割って、計算結果の少数を切り上げ
+total_minutes = math.ceil((quantity * time_per_unit/ 60) #合計秒数を60で割って、計算結果の少数を切り上げ
 
 print(f"1個当たり{time_per_unit}秒かかる工程を{quantity}個では、{total_minutes}分が必要になります。")
